f0091f39fbd128be597792573cc8f90724a798de
SantiagoDarnes <santi.darnes@gmail.com>
2023-09-08 17:44:19 -0300

    Comenzamos la lamparita

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,5 +1,29 @@
+
 public class Lamparita {
-    public static void main(String[] args) throws Exception {
+
+    private boolean encendido = false;
+
+    public Lamparita() {
+    }
+
+    public void encender() {
+        this.encendido = true;
+    }
+
+    public void apagar() {
+        this.encendido = false;
+    }
+
+    public void isEncendido() {
+        if (encendido) {
+            System.out.println("La lamparita esta encendida");
+        }
+        else {
+            System.out.println("La lamparita esta apagada");
+        }
+    }
+
+    public static void main(String[] args) {
         System.out.println("Vamos a programar una lamparita! ");
     }
 }
